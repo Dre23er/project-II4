fbec9f3dd13e9744f4a32feb502b74bf7d393c62
Dre23er <79549421+Dre23er@users.noreply.github.com>
2022-08-26 08:33:33 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(400,400);
+    canvas.center();
+    video = createCapture(VIDEO); 
+    video.hide();
+}
+
+
+function draw(){
+ 
+ image(video,50,50,300,300);
+
+     fill(255,0,0)    
+     ellipse(50,50,50,50);
+
+    fill(255,0,0)    
+    ellipse(50,350,50,50);
+
+    fill(255,0,0)    
+    ellipse(350,350,50,50);
+
+    fill(255,0,0)    
+    ellipse(350,50,50,50);
+        
+    fill(255,0,0)    
+    ellipse(50,50,50,50);
+
+     fill(0,255,0)
+     rect(25,55,50,290,300);
+
+    fill(0,255,0)
+    rect(325,55,50,290,300);
+
+    fill(0,255,0)
+    rect(50,25,300,50,55);
+
+    fill(0,255,0)
+    rect(50,325,300,50,55);
+}
+
+function take_snapshot(){
+save("i_love_food.png");
+}
